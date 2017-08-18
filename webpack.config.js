9f0729f86a555766b23d27d0d693a63f93dbabcb
Minh Nguyen <kuroemon95@gmail.com>
2017-08-17 23:34:23 -0500

    add babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+var path = require('path')
 
 module.exports = {
-  context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./client/index.js",
   output: {
-    path: __dirname + "/js",
-    filename: "scripts.min.js"
+    path: path.resolve(__dirname, "/dist"),
+    filename: "bundle.min.js"
   },
+  module: {
+  rules: [
+    { test: /\.(js|jsx)$/, use: 'babel-loader', exclude: /node_modules/ },
+  ],
+  loaders: [
+    { test: /\.css$/, loader: 'style-loader!css-loader' },
+  ],
+},
   plugins: debug ? [] : [
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
